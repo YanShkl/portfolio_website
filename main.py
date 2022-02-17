92a5476a57856768d2e93a0e641b72b30a68bc92
YanShkl <yshkleinik@gmail.com>
2022-02-17 19:29:38 +0300

    SMTP  change

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ import os
 app = Flask(__name__)
 
 
-SMTP_SERVER = "smtp.gmail.com"
-PORT = 465
+
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 MY_EMAIL = os.environ.get('MY_EMAIL')
 MY_PASSWORD = os.environ.get('MY_PASSWORD')
 RECEIVING_EMAIL = os.environ.get('RECEIVING_EMAIL')
 
-
-
+SMTP_SERVER = "smtp.mail.yahoo.com"
+PORT = 587
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -26,22 +25,28 @@ def main():
 
 @app.route('/send_message', methods=["GET", "POST"])
 def send_message():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        subject = request.form['subject']
-        message = request.form['message']
+    try:
+        if request.method == 'POST':
+            name = request.form['name']
+            email = request.form['email']
+            subject = request.form['subject']
+            message = request.form['message']
 
-        with smtplib.SMTP_SSL(SMTP_SERVER, PORT) as connection:
-            connection.login(MY_EMAIL, MY_PASSWORD)
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=RECEIVING_EMAIL,
-                msg=f"Subject:{subject}\n\nName:{name}\nEmail{email}\nMessage: {message}"
-            )
+            with smtplib.SMTP(SMTP_SERVER, PORT) as connection:
+                connection.starttls()
+                connection.login(MY_EMAIL, MY_PASSWORD)
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=RECEIVING_EMAIL,
+                    msg=f"Subject:{subject}\n\nName: {name}\nEmail: {email}\nMessage: {message}"
+                )
 
-        flash(u"Message was Successfully Sent!", 'success')
-        return redirect(url_for('main'))
+            flash(u"Message was Successfully Sent!", 'success')
+            return redirect(url_for('main'))
+    except:
+            flash(u"Message was  Sent Unsuccessfully!", 'fail')
+            return redirect(url_for('main'))
+
 
 
 if __name__ == "__main__":
